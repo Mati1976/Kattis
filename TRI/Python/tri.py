ba1aacdae565820121ba7f062489d1e708fc1083
Mati Rachamim <54856523+Mati1976@users.noreply.github.com>
2024-08-15 21:42:00 +0200

    Corrected the last elif conditions in TRI.py

--- TRI/Python/tri.py
+++ TRI/Python/tri.py
@@ -9,9 +9,9 @@ elif nums[0] == nums[1] / nums[2] :
     print(str(nums[0]) +'='+ str(nums[1]) +'/'+ str(nums[2]))
 elif nums[0] * nums[1] == nums[2] :
     print(str(nums[0]) +'*'+ str(nums[1]) +'='+ str(nums[2]))
-elif nums[0] + nums[1] + nums[2] :
+elif nums[0] + nums[1] == nums[2] :
     print(str(nums[0]) +'+'+ str(nums[1]) +'='+ str(nums[2]))
-elif nums[0] - nums[1] - nums[2] :
+elif nums[0] - nums[1] == nums[2] :
     print(str(nums[0]) +'-'+ str(nums[1]) +'='+ str(nums[2]))
-elif nums[0] / nums[1] / nums[2] :
+elif nums[0] / nums[1] == nums[2] :
     print(str(nums[0]) +'/'+ str(nums[1]) +'='+ str(nums[2]))
